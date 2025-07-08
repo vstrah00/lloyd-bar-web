a193ed04ca0b87fc5e4cb6ae8c3b269239b1e563
Vito Strahinjic <vitostrah007@gmail.com>
2025-07-08 22:45:27 +0200

    lastmin update to follow specification

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import BlogCard, { BlogTypeCard } from "@/components/BlogCard";
 import LandingSection from "@/components/LandingSection";
 import LandingContainer from "@/components/LandingContainer";
 import CheersSection from "@/components/CheersSection";
+import Link from "next/link";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
   const query = (await searchParams).query;
@@ -19,16 +20,23 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
 
       <CheersSection/>
       <section className="section_container">
-        <p className="text-30-semibold">{query ? `Search results for "${query}"` : `All Posts`}</p>
-        <SearchForm query={query} />
-        <ul className="mt-7 card_grid">
-          {posts?.length > 0 ? (
-            posts.map((post: BlogTypeCard) => <BlogCard key={post?._id} post={post} />)
-          ) : (
-            <p>No valid search</p>
-          )}
-        </ul>
-      </section>
+  <p className="text-30-semibold">{query ? `Search results for "${query}"` : `Recent Posts`}</p>
+  <SearchForm query={query} />
+  <ul className="mt-7 card_grid">
+    {posts?.length > 0 ? (
+      posts.slice(0, 3).map((post: BlogTypeCard) => <BlogCard key={post?._id} post={post} />)
+    ) : (
+      <p>No valid search</p>
+    )}
+  </ul>
+
+  <div className="mt-10 text-right">
+    <Link href="/blogPage" className="startup-card_btn mt-6 px-4 py-2 border rounded">
+      View All Blogs
+    </Link>
+  </div>
+</section>
+
     </>
   );
 }
